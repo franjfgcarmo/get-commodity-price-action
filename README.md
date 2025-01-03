21add97e5972dcfd471823f120113988920d03be
Francisco José Fernández García <francisco.fernandez@terraminium.com>
2025-01-03 19:24:18 +0100

    Created github action

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # get-commodity-price-action
-get-commodity-price-action
+This action gets gold or silver current price per ounce in USD/EUR.
+
+## Inputs
+
+### `commodity`
+
+**Required** The name of the commodity. Default `gold`
+
+### `currency`
+
+**Required** The desired currency. Default `USD`
+
+## Outputs
+
+### `price`
+
+Current price of the selected commodity per ounce
+
+## Example usage
+
+```yaml
+uses: franjfgcarmo/get-commodity-price-action@1.0.0
+with:
+  commodity: 'silver'
+  currency: 'EUR'
+```
+
---
+++ index.js
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try{ 
+    const gold = 'gold';   
+    const silver = 'silver';
+    const USD = 'USD';
+    const EUR = 'EUR';
+    const commodity = core.getInput('commodity').toLowerCase();
+    const currency = core.getInput('price').toUpperCase;
+    const message = `The price of ${commodity} is ${currency}`;
+    console.log(message);
+    let price = 0;
+
+    if(commodity !== gold && commodity !== silver){
+        console.log(`Commodity ${commodity} not supported`);
+    }
+
+    if(commodity === gold){
+        price = currency.toUpperCase() === USD ? 2652.84 : 2425.52;
+    }else if(commodity === silver){
+        price = currency.toUpperCase() === USD ? 30.59 : 29.10;
+    }
+    console.log(`Current price of gold per ounce: ${price} ${currency}`);
+    
+    core.setOutput('price', price);
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+    
+}catch(error){
+    core.setFailed(error.message);
+}
